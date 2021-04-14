c66de6705f1e90600612a5daf884616963cd987d
unknown <captainvee3@gmail.com>
2021-04-14 11:22:13 +0100

    binary search

--- binary_search.py
+++ binary_search.py
@@ -1,18 +1,28 @@
 
 def binary_search(lists, target):
-    pass
+    first = 0
+    last = len(lists) - 1
+    
+    while first <= last:
+        midpoint = (first + last)//2
+        if lists[midpoint] == target:
+            return midpoint
+        elif lists[midpoint] < target:
+            
+            first = midpoint + 1  
+        else:
+            last = midpoint - 1 
+
+    return None
 
 
+def verify(index):
+    if index is not None:
+        print('\nTarget found at index', index)
+    else:
+        print('Target not found')
+        
+numbers = [1,2,3,4,5,6,7,8,9,10]
 
-
-
-
-
-
-
-
-
-
-
-
-
+result = binary_search(numbers, 6)
+verify(result)
